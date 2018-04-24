33b27e18ed5b41b5baa6c977b514fc2d8650949b
Ronan Dineen <ronan@ematix19.ematix>
2018-04-24 15:42:31 +0200

    first commit

--- server.js
+++ server.js
@@ -1,40 +1,50 @@
 //require the express nodejs module
-var express = require('express'),
-	//set an instance of exress
-	app = express(),
-	//require the body-parser nodejs module
-	bodyParser = require('body-parser'),
-	//require the path nodejs module
-	path = require("path");
-	
+var express = require("express"),
+  //set an instance of exress
+  app = express(),
+  //require the body-parser nodejs module
+  bodyParser = require("body-parser"),
+  //require the path nodejs module
+  path = require("path");
+
 //support parsing of application/json type post data
 app.use(bodyParser.json());
 
 //support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //tell express that www is the root of our public web folder
-app.use(express.static(path.join(__dirname, 'www')));
+app.use(express.static(path.join(__dirname, "www")));
 
 //tell express what to do when the /form route is requested
-app.post('/form',function(req, res){
-	res.setHeader('Content-Type', 'application/json');
+app.post("/form", function(req, res) {
+  res.setHeader("Content-Type", "application/json");
 
-	//mimic a slow network connection
-	setTimeout(function(){
+  //mimic a slow network connection
+  setTimeout(function() {
+    res.send(
+      JSON.stringify({
+        firstName: req.body.firstName || null,
+        lastName: req.body.lastName || null,
+        emailAddress: req.body.emailAddress || null
+      })
+    );
+  }, 1000);
 
-		res.send(JSON.stringify({
-			firstName: req.body.firstName || null,
-			lastName: req.body.lastName || null
-		}));
-
-	}, 1000)
-
-	//debugging output for the terminal
-	console.log('you posted: First Name: ' + req.body.firstName + ', Last Name: ' + req.body.lastName);
+  //debugging output for the terminal
+  console.log(
+    "you posted: First Name: " +
+      req.body.firstName +
+      ", Last Name: " +
+      req.body.lastName +
+      ", Email Address:" +
+      req.body.emailAddress
+  );
 });
 
 //wait for a connection
-app.listen(3000, function () {
-  console.log('Server is running. Point your browser to: http://localhost:3000');
+app.listen(3000, function() {
+  console.log(
+    "Server is running. Point your browser to: http://localhost:3000"
+  );
 });
--- www/js/form-handler.js
+++ www/js/form-handler.js
@@ -1,47 +1,46 @@
-(function () {
-
-  function init(){
-    $('#submitButton').click(submitButtonHandler);
+(function() {
+  function init() {
+    $("#submitButton").click(submitButtonHandler);
   }
 
-  function submitButtonHandler (evt) {
-     var testForm = document.getElementById('testForm');
+  function submitButtonHandler(evt) {
+    var testForm = document.getElementById("testForm");
 
-      //prevent form submission
-      evt.preventDefault();
-      evt.stopPropagation();
+    //prevent form submission
+    evt.preventDefault();
+    evt.stopPropagation();
 
-      $('#post-results-container').fadeOut();
-      $('.ajaxLoader').css('display', 'inline-block');
+    $("#post-results-container").fadeOut();
+    $(".ajaxLoader").css("display", "inline-block");
 
-
-      //make the AJAX call
-      $.ajax({
-        url: '/form',
-        type: 'POST',
-        data: {
-          firstName: testForm.firstName.value,
-          lastName: testForm.lastName.value
-        },
-        success: postSuccessHandler
-      });
+    //make the AJAX call
+    $.ajax({
+      url: "/form",
+      type: "POST",
+      data: {
+        firstName: testForm.firstName.value,
+        lastName: testForm.lastName.value,
+        emailAddress: testForm.emailAddress.value
+      },
+      success: postSuccessHandler
+    });
   }
 
-  function postSuccessHandler (jsonData) {
-    var $data = $('#post-results-container .data');
+  function postSuccessHandler(jsonData) {
+    var $data = $("#post-results-container .data");
 
     //reset the UI
-    $data.html('');
-    $('.ajaxLoader').hide();
+    $data.html("");
+    $(".ajaxLoader").hide();
 
-    //update the UI with the data returned from the AJAX call 
-    $.each(jsonData, function (key, val) {
-      $data.append('<li><b>' +  key + '</b>'   + val + '</li>');
+    //update the UI with the data returned from the AJAX call
+    $.each(jsonData, function(key, val) {
+      $data.append("<li><b>" + key + "</b>" + val + "</li>");
     });
 
-    $('#post-results-container').fadeIn();
-  };
+    $("#post-results-container").fadeIn();
+  }
 
-//init on document ready
-$(document).ready(init);
+  //init on document ready
+  $(document).ready(init);
 })();
